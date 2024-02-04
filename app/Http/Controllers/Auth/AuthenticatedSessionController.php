82ab4fffb512824633d56f0c6ac10fafc64cfed1
AsmaNabilah <147595167+citraaldn@users.noreply.github.com>
2024-02-04 16:13:38 +0700

    redirect utang

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect('/utang');
+        //return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
